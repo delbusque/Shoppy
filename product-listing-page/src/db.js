59ffc17541ac046c606a4f5bea67d21b628d3cd8
delbusque <del.busque@gmail.com>
2023-08-04 12:13:23 +0300

    Create db.js

---
+++ product-listing-page/src/db.js
@@ -0,0 +1,62 @@
+const db = [
+    {
+        "id": 2,
+        "name": "Micro Fleece",
+        "desc": "Berghaus Mens Prism Micro Fleece 1/​2 Zip Blue/​Blue",
+        "price": 25.89,
+        "discountPercentage": 12.76,
+        "rating": 1,
+        "category": "jackets",
+        "img": "https://resources.mandmdirect.com/Images/_default/b/e/3/be30300_1_cloudzoom.jpg",
+
+    },
+    {
+        "id": 3,
+        "name": "Shiny Jacket",
+        "desc": "French Connection Mens Row Shiny Jacket Black",
+        "price": 25.89,
+        "discountPercentage": 55.76,
+        "rating": 3,
+        "category": "jackets",
+        "img": "https://resources.mandmdirect.com/Images/_default/n/n/3/nn30748_1_cloudzoom.jpg",
+
+    },
+    {
+        "id": 1,
+        "name": "half sleeves T shirts",
+        "desc": "Many store is creating new designs and trend every month and every year",
+        "price": 23,
+        "discountPercentage": 12.76,
+        "rating": 2,
+        "category": "mens-shirts",
+        "img": "https://i.dummyjson.com/data/products/51/2.jpg",
+
+    },
+    {
+        "id": 2,
+        "name": "Micro Fleece",
+        "desc": "Berghaus Mens Prism Micro Fleece 1/​2 Zip Blue/​Blue",
+        "price": 25.89,
+        "discountPercentage": 12.76,
+        "rating": 4,
+        "category": "jackets",
+        "img": "https://resources.mandmdirect.com/Images/_default/b/e/3/be30300_1_cloudzoom.jpg",
+
+    },
+    {
+        "id": 3,
+        "name": "Shiny Jacket",
+        "desc": "French Connection Mens Row Shiny Jacket Black",
+        "price": 25.89,
+        "discountPercentage": 55.76,
+        "rating": 5,
+        "category": "jackets",
+        "img": "https://resources.mandmdirect.com/Images/_default/n/n/3/nn30748_1_cloudzoom.jpg",
+
+    }
+
+
+
+]
+
+export default db;
