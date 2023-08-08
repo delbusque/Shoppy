bb5690e13437a737b9429f8fa9ab956fa377c650
Ventsislav Peychev <del.busque@gmail.com>
2023-08-08 13:06:21 +0300

    Added responsive design down to max-width 1100px

--- product-listing-page/src/components/main/sort/Sort.js
+++ product-listing-page/src/components/main/sort/Sort.js
@@ -6,7 +6,7 @@ const Sort = ({ sortHandler, showed, filtered }) => {
         <div className={styles['sort-cont']}>
             <div className={styles['counter']}>{showed} of {filtered} products</div>
             <select name="sort" className={styles['select']} onChange={(e) => sortHandler(e)}>
-                <option disabled selected>Sort products by:</option>
+                <option disabled selected value="selected">Sort products by:</option>
                 <option value="a-z" >Name (A-Z)</option>
                 <option value="z-a">Name (Z-A)</option>
                 <option value="asc" >Price (Low-High)</option>
