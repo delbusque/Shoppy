ed4a39937306f62036304b05db86e43cc8f222f6
Ventsislav Peychev <del.busque@gmail.com>
2023-08-08 08:50:37 +0300

    Fixed sort and tiles styling

--- product-listing-page/src/components/main/sort/Sort.js
+++ product-listing-page/src/components/main/sort/Sort.js
@@ -6,11 +6,11 @@ const Sort = ({ sortHandler, showed, filtered }) => {
         <div className={styles['sort-cont']}>
             <div className={styles['counter']}>{showed} of {filtered} products</div>
             <select name="sort" className={styles['select']} onChange={(e) => sortHandler(e)}>
-                <option disabled value="selected" selected>Sort:</option>
-                <option value="a-z" >A-Z</option>
-                <option value="z-a">Z-A</option>
-                <option value="asc">Low-High</option>
-                <option value="desc">High-Low</option>
+                <option disabled value="selected" selected>Sort products by:</option>
+                <option value="a-z" >Name (A-Z)</option>
+                <option value="z-a">Name (Z-A)</option>
+                <option value="asc">Price (Low-High)</option>
+                <option value="desc">Price (High-Low)</option>
             </select>
         </div>
     )
