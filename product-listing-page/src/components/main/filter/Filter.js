18a4eeb2d26cf806602791635d5064591cbd8462
Ventsislav Peychev <del.busque@gmail.com>
2023-08-09 11:47:43 +0300

    Added responsive design for filter and category components

--- product-listing-page/src/components/main/filter/Filter.js
+++ product-listing-page/src/components/main/filter/Filter.js
@@ -2,9 +2,21 @@ import Color from './Color.js';
 import styles from './Filter.module.css'
 import Price from './Price.js';
 
-const Filter = ({ setByPrice, setByColor }) => {
+const Filter = ({ setByPrice, setByColor, categoryHandler }) => {
     return (
         <div className={styles['filter-cont']}>
+
+            <ul className={styles["cat-cont"]}>
+
+                <li className={styles['cat']} onClick={(e) => categoryHandler(e)}>Jackets</li>
+                <li className={styles['cat']} onClick={(e) => categoryHandler(e)}>Trousers</li>
+                <li className={styles['cat']} onClick={(e) => categoryHandler(e)}>T-Shirts</li>
+                <li className={styles['cat']} onClick={(e) => categoryHandler(e)}>Shorts</li>
+                <li className={styles['cat']} onClick={(e) => categoryHandler(e)}>Shoes</li>
+
+            </ul>
+
+
 
             <Price setByPrice={setByPrice} />
             <Color setByColor={setByColor} className={styles['color']} />
